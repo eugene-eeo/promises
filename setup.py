d615c9dd5739a4b42b920542c5b916ca44e212dd
Eugene Eeo <xe0jun@hotmail.com>
2014-01-18 18:54:53 +0800

    modified setup.py, waiting for pypi to go online

--- setup.py
+++ setup.py
@@ -2,29 +2,11 @@ from distutils.core import setup
 
 setup(
     name="Promises",
-    version="0.0.2",
+    version="0.0.3",
     description="Python Type Checking",
     author="Eugene Eeo",
     author_email="packwolf58@gmail.com",
     packages=["promises"],
-    long_description="""
-Promises is a little Python toolkit for maintaining
-some sanity when doing testing or building an API in
-dynamically typed languages. Easily declare the
-signature of your functions like so::
-
-    from promises import accepts, rejects
-
-    @accepts(int)
-    @rejects(int)
-    def f(x):
-        return x+1
-
-Promises only exposes a very minimal declarative
-and decorator-based API that is inspired by
-function signatures in Go, and is very easy to
-learn. You can get more documentation of the
-project by visiting `github.com/eugene-eeo/promises
-<https://github.com/eugene-eeo/promises>`_."""
+    long_description=open("README.rst").read()
         )
 
