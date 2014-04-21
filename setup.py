46287fab95b4daebbcdb8953ee9adc2cdec93806
Eugene Eeo <xe0jun@hotmail.com>
2014-04-21 21:12:18 +0800

    bump version to 0.1.1 to reflect changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="Promises",
-    version="0.1.0",
+    version="0.1.1",
     description="Python Type Checking",
     author="Eugene Eeo",
     author_email="packwolf58@gmail.com",
