38381fd823a3f2c5a9e561fd1ab9076d47a26f51
Eugene Eeo <xe0jun@hotmail.com>
2014-04-26 22:56:43 +0800

    fix versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="Promises",
-    version="0.1.1",
+    version="0.6.15",
     description="Python Type Checking",
     author="Eugene Eeo",
     author_email="packwolf58@gmail.com",
