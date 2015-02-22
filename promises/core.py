13f8b55b7c138d7038ee641f7dda276deb17aa06
Eugene Eeo <xe0jun@hotmail.com>
2015-02-22 17:26:27 +0800

    bugfixes

--- promises/core.py
+++ promises/core.py
@@ -1,31 +1,9 @@
 from functools import wraps
+from promises.utils import signature, validate_kwdargs, validate_posargs
 
 
 EXPECT_ARG = 'Expected argument %r to be %r'
 EXPECT_RET = 'Expected return value to be %r'
-
-
-def signature(f):
-    if not hasattr(f, '__orgargs__'):
-        code = f.__code__
-        f.__orgargs__ = code.co_varnames[:code.co_argcount]
-    return f.__orgargs__
-
-
-def validate_posargs(args, types):
-    for val, _type in zip(args, types):
-        if not isinstance(val, _type):
-            raise TypeError(EXPECT_ARG % (val, _type))
-
-
-def validate_kwdargs(args, types):
-    for arg in args:
-        if arg not in types:
-            continue
-        value = args[arg]
-        type_ = types[arg]
-        if not isinstance(value, type_):
-            raise TypeError(EXPECT_ARG % (value, type_))
 
 
 def accepts(*types, **index):
